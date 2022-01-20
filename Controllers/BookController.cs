48f36a085e9a0553d8da1a9a879063d82e9f89f0
Patrycja Smolińska <54121426+PatrycjaSmol@users.noreply.github.com>
2022-01-20 10:13:34 +0100

    Add files via upload
    Dodanie pliku Controllers

---
+++ Controllers/BookController.cs
@@ -0,0 +1,40 @@
+﻿using BookList1.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace BookList1.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task <IActionResult> GetAll() 
+        {
+            return Json(new { data =await _db.Book.ToListAsync()});
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            else
+                _db.Book.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
+            
+        }
+    }
+}
